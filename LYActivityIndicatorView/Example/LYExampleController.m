2cf730b26354c69f4d70b8693f99a88e3069157e
Carlos Vidal <nakioparkour@gmail.com>
2015-10-09 15:25:07 +0100

    built sample ui on interface builder

--- LYActivityIndicatorView/Example/LYExampleController.m
+++ LYActivityIndicatorView/Example/LYExampleController.m
@@ -9,43 +9,10 @@
 #import "LYExampleController.h"
 #import "LYActivityIndicatorView.h"
 
-@interface LYExampleController () <UIWebViewDelegate>
-
-@property (weak, nonatomic) IBOutlet UIWebView *webView;
-@property (weak, nonatomic) IBOutlet UIView *alertContainerView;
-@property (weak, nonatomic) IBOutlet LYActivityIndicatorView *activityIndicatorView;
+@interface LYExampleController ()
 
 @end
 
 @implementation LYExampleController
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.lyst.com"]];
-    [self.webView setDelegate:self];
-    [self.webView loadRequest:request];
-}
-
-- (BOOL)prefersStatusBarHidden {
-    return YES;
-}
-
-#pragma mark - UIWebViewDelegate methods
-
-- (void)webViewDidStartLoad:(UIWebView *)webView {
-    [self.alertContainerView setHidden:NO];
-    [self.activityIndicatorView startAnimating];
-}
-
-- (void)webViewDidFinishLoad:(UIWebView *)webView {
-    [self.alertContainerView setHidden:YES];
-    [self.activityIndicatorView stopAnimating];
-}
-
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
-    [self.alertContainerView setHidden:YES];
-    [self.activityIndicatorView stopAnimating];
-}
 
 @end
