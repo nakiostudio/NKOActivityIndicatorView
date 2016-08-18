b22876e9c33f9e985b01f84280a2d006727ac061
Carlos Vidal <nakioparkour@gmail.com>
2016-08-18 16:39:10 +0100

    created framework target

---
+++ NKOActivityIndicatorView/NKOActivityIndicatorView-Umbrella.h
@@ -0,0 +1,19 @@
+//
+//  NKOActivityIndicatorView.h
+//  NKOActivityIndicatorView
+//
+//  Created by Carlos Vidal Pallin on 18/08/2016.
+//  Copyright © 2016 Carlos Vidal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NKOActivityIndicatorView.
+FOUNDATION_EXPORT double NKOActivityIndicatorViewVersionNumber;
+
+//! Project version string for NKOActivityIndicatorView.
+FOUNDATION_EXPORT const unsigned char NKOActivityIndicatorViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NKOActivityIndicatorView/PublicHeader.h>
+
+
